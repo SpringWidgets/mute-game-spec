8928ec755e97df6f309eba263a6382b8906a3338
Alexander Lipatov <alexlpt@yandex.ru>
2015-04-19 22:41:36 +0300

    swith from FPS based decision to GameProgress based

--- mute_game_spec.lua
+++ mute_game_spec.lua
@@ -1,12 +1,12 @@
 function widget:GetInfo()
     return {
-        name      = "Mute Game Spec v1.1",
+        name      = "Mute Game Spec v1.2",
         desc      = "Disables sound when you start spectating battle (you can enable with F6)",
         author    = "[teh]decay aka [teh]undertaker aka [DoR]Saruman & jetbird",
         date      = "18 apr 2015",
         license   = "The BSD License",
         layer     = 0,
-        version   = 1.1,
+        version   = 1.2,
         enabled   = true  -- loaded by default
     }
 end
@@ -15,33 +15,56 @@ end
 -- project page on github: https://github.com/SpringWidgets/mute-game-spec
 
 --Changelog
--- v 1.1 added automatic unmute detector
--- v 1 initial version
+
+-- v 1.2 [teh]decay: swith from FPS based decision to GameProgress based
+-- v 1.1    jetbird: added automatic unmute detector
+-- v 1   [teh]decay: initial version
 
 
 local spGetMyPlayerID      = Spring.GetMyPlayerID
 local spGetPlayerInfo      = Spring.GetPlayerInfo
 local spSendCommands       = Spring.SendCommands
 local spEcho               = Spring.Echo
-local spGetFPS             = Spring.GetFPS
+local spGetTimer           = Spring.GetTimer
+local spDiffTimers         = Spring.DiffTimers
+local spGameSpeedFactor    = Spring.GetGameSpeed
 
-local goodFPStime = 0;
+local serverFrameNum_G = -1;
+local initTimer;
+local gameFrame = -1;
 
-function widget:Update(dt)
-    if spGetFPS() > 20 then
-        goodFPStime = goodFPStime + dt;
-        if goodFPStime > 2.0 then
+function widget:GameProgress(serverFrameNum)
+    serverFrameNum_G = serverFrameNum;
+end
+
+function widget:GameFrame(myGameFrame)
+    gameFrame = myGameFrame
+    if serverFrameNum_G >= 0 then
+        local frameDistanceToFinish = serverFrameNum_G - myGameFrame
+
+        if frameDistanceToFinish < 1 then
             spSendCommands("mutesound")
             widgetHandler:RemoveWidget()
         end
-    else
-        goodFPStime = 0
     end
 end
+
+-- if game was not fast forwarded 5 seconds - then we are synced
+function widget:DrawScreen()
+    local currTimer = spGetTimer()
+
+    if spDiffTimers(currTimer, initTimer) >= 8 and gameFrame < 0 then
+        spSendCommands("mutesound")
+        widgetHandler:RemoveWidget()
+    end
+end
+
 
 function widget:Initialize()
     local playerID = spGetMyPlayerID()
     local _, _, spec, _, _, _, _, _ = spGetPlayerInfo(playerID)
+
+    initTimer = spGetTimer()
 
     if ( spec == true ) then
         spSendCommands("mutesound")
